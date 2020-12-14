ea1385d6918a1c89fff238ddcf3b48477583944e
christopheveloupoule <christopheveloupoule@yahoo.fr>
2020-12-14 21:49:31 +0100

    git tag step-3

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,6 +1,6 @@
 // capitalizeFirstLetters.js
 
 const capitalizeFirstLetters = (string) => 
-    (string.length === 0) ? "" : string.split(' ').map(s => s[0].toUpperCase() + s.substr(1)).join(' ');
+    (string.length === 0) ? "" : string.split(' ').map(s => s[0].toUpperCase() + s.substr(1)).join(' '); 
 
 module.exports = capitalizeFirstLetters;
